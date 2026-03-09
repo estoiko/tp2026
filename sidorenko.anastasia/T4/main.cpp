cc31c26f84446942099f7e97754eab67ffce38bb
sidorenkoanastasia-ca <sidorenkoanastasia.ca@gmail.com>
2026-03-09 22:44:54 +0300

    leave only shapes

--- sidorenko.anastasia/T4/main.cpp
+++ sidorenko.anastasia/T4/main.cpp
@@ -61,17 +61,11 @@ int main()
     composite->addShape(std::make_unique<RightTrapezoid>(Point(9, 3), 2, 1, 3));
 
     shapes.push_back(std::move(composite));
-    std::cout << "---------------------\n";
-    std::cout << "SHAPES BEFORE SCALE\n";
-    std::cout << "----------------------\n";
     printInfo(shapes);
     for (size_t i = 0; i < shapes.size(); ++i)
     {
         shapes[i]->scale(2);
     }
-    std::cout << "------------------------\n";
-    std::cout << "SHAPES AFTER SCALE\n";
-    std::cout << "-------------------------\n";
     printInfo(shapes);
     return 0;
 }
