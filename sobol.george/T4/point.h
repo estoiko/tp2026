4e6780e8edcbcc04911829982793823800c62905
JORJIMSI\zhora <zhora.sobol.2017@mail.ru>
2026-03-12 17:11:58 +0300

    attempt 2

--- sobol.george/T4/point.h
+++ sobol.george/T4/point.h
@@ -6,43 +6,43 @@
 class Point
 {
 public:
-    double x;
-    double y;
-    Point() : x(0.00), y(0.00) {}
-    Point(const double& x_, const double& y_) : x(x_), y(y_) {}
-    Point(const Point& ot) : x(ot.x), y(ot.y) {}
-    Point(Point&& ot) : x(ot.x), y(ot.y)
-    {
-        ot.x = 0;
-        ot.y = 0;
-    }
-    ~Point() {}
-
-    Point& operator=(const Point& ot)
-    {
-        if (this != &ot)
-        {
-            x = ot.x;
-            y = ot.y;
+double x;
+double y;
+Point() : x(0.00), y(0.00) {}
+Point(const double& x_, const double& y_) : x(x_), y(y_) {}
+Point(const Point& ot) : x(ot.x), y(ot.y) {}
+Point(Point&& ot) : x(ot.x), y(ot.y)
+{
+ot.x = 0;
+ot.y = 0;
 }
-        return *this;
-    }
-    Point& operator=(Point&& ot)
-    {
-        if (this != &ot)
-        {
-            x = ot.x;
-            y = ot.y;
-            ot.x = 0;
-            ot.y = 0;
-        }
-        return *this;
-    }
-    void print()
-    {
-        std::cout<< "( " << x << " ; " << y << " )\n";
-    }
+~Point() {}
+
+Point& operator=(const Point& ot)
+{
+if (this != &ot)
+{
+x = ot.x;
+y = ot.y;
+}
+return *this;
+}
+Point& operator=(Point&& ot)
+{
+if (this != &ot)
+{
+x = ot.x;
+y = ot.y;
+ot.x = 0;
+ot.y = 0;
+}
+return *this;
+}
+void print()
+{
+std::cout<< "( " << x << " ; " << y << " )\n";
+}
 };
 
 
-#endif 
+#endif
